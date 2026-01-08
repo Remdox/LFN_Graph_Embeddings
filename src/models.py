b448ac51d3f0af08a67e5916fa0838952c87a375
Kairor.95 <118349608+Kairos95@users.noreply.github.com>
2026-01-08 18:06:19 +0100

    SVM

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from sklearn import svm
 
 class Model(ABC):
     def __init__(self):
@@ -6,19 +7,29 @@ class Model(ABC):
         # TODO decidere gli attributi
 
     @abstractmethod
-    def train(self):
+    def train(self, X, y):
         pass
 
     @abstractmethod
-    def predict(self):
+    def predict(self, X):
         pass
+
+class SVM(Model):
+    def __init__(self):
+        self.model = svm.SVC(kernel="linear", C=1e4)
+
+    def train(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
 
 class MLP(Model):
     def __init__(self):
         print("ok")
 
-    def train(self):
+    def train(self, X, y):
         pass
 
-    def predict(self):
+    def predict(self, X):
         pass
